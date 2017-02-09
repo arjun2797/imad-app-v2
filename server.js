77271491c0fa2041b91a7119cb7e20d3fd655a4e
Nagarjun <arjun2797@gmail.com>
2017-02-09 18:56:34 +0530

    Update server.js

--- server.js
+++ server.js
@@ -14,6 +14,10 @@ app.get('/ui/style.css', function (req, res) {
 });
 
 
+app.get('/ui/bg.png', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'bg.png'));
+});
+
 app.get('/ui/logo.png', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'logo.png'));
 });
