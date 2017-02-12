f72bcd56820ff022ce5ab615c22eacc11c960b21
Nagarjun <arjun2797@gmail.com>
2017-02-12 18:07:23 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,21 +1,28 @@
-var button= document.getElementById('counter');
-button.onClick= function(){
+var button = document.getElementById('counter');
 
-    var request=new XMLHttpRequest(); 
-    
-    request.onreadystatechange = function()
+
+
+button.onclick = function()
+{
+
+
+   var request = new XMLHttpRequest();
+
+
+request.onreadystatechange = function ()
+{
+    if(request.readyState === XMLHttpRequest.DONE)
     {
-        if (request.readyState === XMLHttpRequest.DONE)
+        if(request.status === 200)
         {
-            if(request.status === 200) {
-                var counter= request.responseText;
-                var span= document.getElementById('count');
-                span.innerHTML= counter.toString();
-            }
+            var counter = request.responseText;
+             var span = document.getElementById('count');
+             span.innerHTML = counter.toString();
+
         }
-    };
-    
-    request.open('GET','http://arjun2797.imad.hasura-app.io/counter', true);
-    request.send(null);
-   
+    }
+
+}
+request.open('GET',"http:/http://arjun2797.imad.hasura-app.io/counter",true);
+request.send(null);
 };
