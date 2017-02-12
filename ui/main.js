7d706c34df24194970f203718548945631fc29b5
Nagarjun <arjun2797@gmail.com>
2017-02-12 17:16:26 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,1 +1,25 @@
 console.log('Loaded!');
+var button= document.getElementById("counter");
+var counter=0;
+button.onClick= function(){
+
+    var req=new XMLHttpRequest(); 
+    
+    req.onreadystatechange = function()
+    {
+        if (req.readystatechange == XMLHttpRequest.DONE)
+        {
+            if(req.status == 200)
+            {
+                var counter= req.responseText;
+                var span=document.getElementById("count");
+                span.innerHTML=counter.toString();
+                
+            }
+        }
+    };
+    
+    req.open('GET','http://arjun2797.cloud.imad.hasura.io/counter',true);
+    req.send(null);
+   
+};
